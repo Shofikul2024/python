5c3e517d2620f36ab0b6210c2fd5d81e97205784
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-24 01:31:50 +0600

    first commit

---
+++ page-25.py
@@ -0,0 +1,6 @@
+number1=input("please type an integer and press enter : ")
+number2=input("please type another an integer and press enter : ")
+number1=int(number1)
+number2=int(number2)
+print("number1+number2=", number1+number2)
+ 
