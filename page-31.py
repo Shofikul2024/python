14424ec966c50bf4586f6dad6f9e1aec6b348186
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-26 01:00:57 +0600

    31 page  added

--- page-31.py
+++ page-31.py
@@ -1,4 +1,5 @@
 saarc =["bangladesh", "india" ,"pakistan" ,"srilanka" , "afganistan", "nepal" ,"bhutan"  ] 
-print(saarc)
-print(saarc[0])
+# print(saarc)
+# print(saarc[0])
 
+print("number of country in saarc:" , len(saarc))
