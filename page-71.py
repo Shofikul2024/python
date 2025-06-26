17127be5c7f29c1fcf8b731c22272383f8f3d786
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-27 01:42:34 +0600

    71 page  added

--- page-71.py
+++ page-71.py
@@ -1,2 +1,8 @@
 saarc =["bangladesh", "india" ,"pakistan" ,"srilanka" , "afganistan", "nepal" ,"bhutan"  ] 
 print(saarc)
+
+saarc.append("Afganistan")
+print(saarc)
+
+saarc.append("America")
+print(saarc)
