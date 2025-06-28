38ef1813484a776a0c4b96948a24dfac4b26e41c
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-28 22:19:00 +0600

    list  added

--- list.py
+++ list.py
@@ -8,7 +8,13 @@
 # print(type(b))
 
 
+# b=['onion' , 'potato ', 'ginger' , 'cucumber' ,1, 3.1416]
+
+# store=b[0]
+# print(store)
+
+
 b=['onion' , 'potato ', 'ginger' , 'cucumber' ,1, 3.1416]
 
-store=b[0]
-print(store)
+b[0]
+print(b[0])
