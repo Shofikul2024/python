85a61396261a77e4b8d12468572ccc3ae0ef693f
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-29 02:30:09 +0600

    added   function

--- page-71.py
+++ page-71.py
@@ -12,11 +12,20 @@
 
  # i am a shofikul islam
 
-saarc =["bangladesh", "india" ,"pakistan" ,"srilanka" , "afganistan", "nepal" ,"bhutan"  ]
-print(saarc)
+# saarc =["bangladesh", "india" ,"pakistan" ,"srilanka" , "afganistan", "nepal" ,"bhutan"  ]
+# print(saarc)
 
-saarc.append("Afganistan")
-print(saarc)
+# saarc.append("Afganistan")
+# print(saarc)
 
-saarc.append("America")
-print(saarc)
+# saarc.append("America")
+# print(saarc)
+
+# saarc =["bangladesh", "india" ,"pakistan" ,"srilanka" , "afganistan", "nepal" ,"bhutan"  ]
+# print(saarc)
+
+# saarc.append("Afganistan")
+# print(saarc)
+
+# saarc.append("America")
+# print(saarc)
