1a6c88836fe76deedf5aed6f0c31f7a3044c07cd
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-28 22:25:02 +0600

    list  added

--- list.py
+++ list.py
@@ -14,7 +14,33 @@
 # print(store)
 
 
+# b=['onion' , 'potato ', 'ginger' , 'cucumber' ,1, 3.1416]
+
+# b[0]
+# print(b[0])
+
+
+
+
+
+'''     amra    onk   vabe  akta problem  solution   korte  pari  '''
+
+'''    python   a  comment         jonno   32  ta   single  or   doubler   cotetion    dite  hoy '''
+
+# b=['onion' , 'potato ', 'ginger' , 'cucumber' ,1, 3.1416]
+
+# # b[0]
+# print(b[1])
+
+
+# same  result  asbe 
+
+# b=['onion' , 'potato ', 'ginger' , 'cucumber' ,1, 3.1416]
+
+# print(b[1])
+
+
 b=['onion' , 'potato ', 'ginger' , 'cucumber' ,1, 3.1416]
 
-b[0]
-print(b[0])
+print(b[3])
+
