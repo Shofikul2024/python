b4bf8745f664603f50f3946bfcdee94bda103a94
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-30 00:10:35 +0600

    added   function

--- tuple.py
+++ tuple.py
@@ -1,2 +1,7 @@
-a = ()
-print (type(a))
+# a = ()
+# print (type(a))
+
+b = ('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
+ 
+('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
+print (type(b))
