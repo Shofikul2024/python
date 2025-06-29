74fe50b5639d791a296eeeaaae05fa661cb0797b
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-30 00:13:03 +0600

    added   tuple

--- tuple.py
+++ tuple.py
@@ -1,7 +1,28 @@
 # a = ()
 # print (type(a))
 
-b = ('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
+
+
+
+
+
+# b = ('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
  
-('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
-print (type(b))
+# ('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
+# print (type(b))
+
+
+
+
+b = ('onion', 'potato', 'ginger', 'cucumber', 1, 3.1416)
+b[0]
+
+# 'potato'
+# >>> b[1:5]
+# ('potato', 'ginger', 'cucumber', 1)
+# >>> b[:5]
+# ('onion', 'potato', 'ginger', 'cucumber', 1)
+# >>> b[2:]
+# ('ginger', 'cucumber', 1, 3.1416)
+
+print(b[0])
