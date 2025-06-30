99136ce76a26b930843c6b7c0c0d0aaf87a9a3a7
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-06-30 16:44:15 +0600

    added   tuple

--- set.py
+++ set.py
@@ -1,2 +1,8 @@
-A = {'orange', 'banana', 'pear', 'apple'}
-print(type(A))
+# A = {'orange', 'banana', 'pear', 'apple'}
+# print(type(A))
+
+
+
+A={'apple', 'orange', 'banana', 'pear'}
+A.add('pineapple')
+print(A)
