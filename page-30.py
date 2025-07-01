c63c822e807ff4a093a57861b3276cf6872ea1bb
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-01 20:50:11 +0600

    added   tuple

--- page-30.py
+++ page-30.py
@@ -1,3 +1,16 @@
+# number_list = [1, 2, 3, 4, 5,6,7,8,9,10]
+# print (number_list)
+# print (number_list[0])
+# print("number of length :" , len(number_list))
+# print( len(number_list))
+
+
+
+
+
+
+
+#practice 
 number_list = [1, 2, 3, 4, 5,6,7,8,9,10]
 print (number_list)
 print (number_list[0])
