4301cf43608e0bcb0584c0f6c7f8eac1bf413236
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-09 23:49:10 +0600

    added input function

--- python-syudy-mart-shakil-vaiya/day-3.py
+++ python-syudy-mart-shakil-vaiya/day-3.py
@@ -10,12 +10,57 @@ input function
 
 
 
+# # type(x)
+
+# print(type(x))
 
 
 
 
+
+
+''' 
 x= int(input("Enter your first  num:"))
 y= int(input("Enter your second  num:"))
 
 print(x+y)
 
+
+'''
+
+
+
+
+
+
+
+
+'''
+x= input()
+y= input()
+
+
+print(x+y)
+
+# x=int("Enter your first  number :")
+# y=int("Enter your second  number :")
+
+ 
+
+
+'''
+
+
+
+
+'''
+
+x=int(input("Enter your first Number:"))
+y=int(input("Enter your second Number:"))
+
+
+print(x+y)
+
+
+
+'''
