e35cf53d65ee090b55edf2373f57099963c912cd
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-09 17:16:59 +0600

    added   day-1 py]

--- class.py
+++ class.py
@@ -1,7 +1,20 @@
+# class myclass :
+#     x=10 
+#     y=30 
+#     z=40 
+#     sum =x+y+z 
+    
+#     print(sum)   
+
+
+
+
+
+
 class myclass :
     x=10 
     y=30 
     z=40 
     sum =x+y+z 
     
-    print(sum)   
+ 
