9f2f5a0fa062f8e67dd8ed26077fe0f165689688
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-13 02:33:48 +0600

    added    first  module  for python

---
+++ conditional-statement.py
@@ -0,0 +1,7 @@
+number = float(input('Please  input the number:'))
+if number > 0:
+    print('Positive')
+elif number < 0:
+    print('Negative')
+else:
+    print('Zero')
---
+++ my-app.py/main.py
@@ -0,0 +1,17 @@
+import calculator
+print(calculator.add(20 ,50))
+print(calculator.sub(100 ,20))
+print(calculator.div(100 ,20))
+print(calculator.mul(10 ,30))
+
+
+
+
+# # main.py
+
+# import calculator
+
+# print(calculator.add(10, 20))   # Output: 30
+# print(calculator.sub(10, 20))   # Output: -10
+# print(calculator.div(10, 20))   # Output: 0.5
+# print(calculator.mul(10, 20))   # Output: 200
