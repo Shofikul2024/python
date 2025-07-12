84adae432946ea86b05ae440e9a30dbe51615193
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-13 02:39:27 +0600

    added    first  module  for python

--- my-app.py/main.py
+++ my-app.py/main.py
@@ -1,13 +1,10 @@
-import calculator
-print(calculator.add(20 ,50))
-print(calculator.sub(100 ,20))
-print(calculator.div(100 ,20))
-print(calculator.mul(10 ,30))
 
 
 
 
-# # main.py
+
+
+# this   code  tsken  chatrgtp
 
 # import calculator
 
@@ -15,3 +12,45 @@ print(calculator.mul(10 ,30))
 # print(calculator.sub(10, 20))   # Output: -10
 # print(calculator.div(10, 20))   # Output: 0.5
 # print(calculator.mul(10, 20))   # Output: 200
+
+
+
+
+
+
+
+
+#   this  code  provide  rabbuil    hasan  
+
+# import calculator
+# print(calculator.add(20 ,50))
+# print(calculator.sub(100 ,20))
+# print(calculator.div(100 ,20))
+# print(calculator.mul(10 ,30))
+
+
+
+
+
+
+
+# only one    function add 
+
+
+# from calculator import add 
+
+
+# print(add(70 ,60))
+
+
+
+
+# more   function   add     
+
+
+from calculator import add , sub ,mul
+
+
+print(add(70 ,60))
+print(sub(70 ,60))
+print(mul(70 ,60))
