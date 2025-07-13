f15dc1c0c5258a9b17b2377ee196bff5e2d8da0f
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-13 18:50:43 +0600

    added    first  module  for python

---
+++ my-app.py/for-loop.py
@@ -0,0 +1,10 @@
+# range(5)
+# range(0, 10)
+# print(list(range(5)))
+
+
+
+range(5, 21)
+range(5, 21)
+
+print(list(range(5, 21)))
--- my-app.py/list.py
+++ my-app.py/list.py
@@ -1,4 +1,19 @@
+# a=['onion',  'potato' , 'ginger' , 'ciucumber' ]
+# print(type(a))
+# for item in a :
+#     print (item)
+
+
+
+
+
+
+
+
 a=['onion',  'potato' , 'ginger' , 'ciucumber' ]
-print(type(a))
+
 for item in a :
     print (item)
+
+
+
