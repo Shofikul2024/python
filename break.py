7d8a5c781bbd00dee0fbddc63e2f5b410564d7d5
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-14 20:17:37 +0600

    added    first  module  for python

---
+++ break.py
@@ -0,0 +1,31 @@
+# habluList=[1,2,3,4,5,6,7,8,9,10 ]
+
+# for x in range(len(habluList)):
+#     if x==4:
+#         break
+#     print(x)
+
+
+
+
+
+       
+habluList=[1,2,3,4,5,6,7,8,9,10 ]
+
+for x in range(len(habluList)):
+    if x==4:
+        break
+    print(x)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
